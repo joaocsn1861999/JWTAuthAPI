9be46ac965ed5dbe4110fbd743f718c2f7bd061e
João Carlos <joaocarlosciclovida@gmail.com>
2025-05-16 16:32:47 -0300

    fix: correcao conexao ou criacao do banco de dados

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,7 +1,18 @@
 import sqlite3 from 'sqlite3';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbFolderPath = path.resolve(__dirname, '../../../database');
+
+if (!fs.existsSync(dbFolderPath)) {
+  fs.mkdirSync(dbFolderPath, { recursive: true });
+}
 
 const db = new sqlite3.Database(
-  '../../../database/database.sqlite',
+  path.join(dbFolderPath, 'database.sqlite'),
   (err) => {
     if (err) console.error('Erro ao conectar ao banco:', err.message);
     else console.log('Conectado ao banco de dados SQLite.');

2fbdff47762ac743dbaa2902c9cdafed27f88f72
João Carlos <joaocarlosciclovida@gmail.com>
2025-05-21 16:12:54 -0300

    update: implementado user validators em user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,14 +1,15 @@
 import UserService from '../services/UserService.js';
+import UserValidators from '../utils/validators/UserValidators.js';
 
 class UserController {
 
   async index(req, res) {
     try {
-        const result = await UserService.findAllWithPagination(
-            req.query.name,
-            req.query.page,
-            req.query.limit
-        );
+        const name = typeof req.query.name === 'string' ? name : '';
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+
+        const result = await UserService.findAllWithPagination(name, page, limit);
         return res.status(200).json({
             message: 'Usuários encontrados com sucesso',
             ...result
@@ -20,6 +21,14 @@ class UserController {
 
   async show(req, res) {
     try {
+        const idValidation = UserValidators.isValidId(req.params.id);
+        if (!idValidation.isValid) {
+            return res.status(400).json({
+                message: 'ID inválido',
+                errors: idValidation.errors
+            });
+        }
+
         const user = await UserService.findById(req.params.id);
         return res.status(200).json({
             message: 'Usuário encontrado com sucesso',
@@ -32,6 +41,14 @@ class UserController {
 
   async store(req, res) {
     try {
+        const userValidation = UserValidators.isValidUserToCreate(req.body);
+        if (!userValidation.isValid) {
+            return res.status(400).json({
+                message: 'Usuário inválido',
+                errors: userValidation.errors
+            });
+        }
+
         const user = await UserService.createUser(req.body);
         return res.status(201).json({
             message: 'Usuário cadastrado com sucesso',
@@ -44,7 +61,17 @@ class UserController {
 
   async update(req, res) {
     try {
-        const user = await UserService.updateUser(req.body, req.params.id);
+        const userData = req.body;
+        const userId = req.params.id;
+        const userValidation = UserValidators.isValidUserToUpdate({userId, ...userData});
+        if (!userValidation.isValid) {
+            return res.status(400).json({
+                message: 'Dados do usuário inválidos',
+                errors: userValidation.errors
+            });
+        }
+
+        const user = await UserService.updateUser(userData, userId);
         return res.status(200).json({
             message: 'Usuário atualizado com sucesso',
             user: user
@@ -56,10 +83,20 @@ class UserController {
 
   async changePassword(req, res) {
     try {
+        const id = req.user.id;
+        const {currentPassword, newPassword} = req.body;
+        const passwordValidation = UserValidators.isValidPasswordChange(currentPassword, newPassword);
+        if (!passwordValidation.isValid) {
+            return res.status(400).json({
+                message: 'Falha ao validar senha',
+                errors: passwordValidation.errors
+            });
+        }
+
         await UserService.updatePassword(
-            req.params.id,
-            req.body.currentPassword,
-            req.body.newPassword
+            id,
+            currentPassword,
+            newPassword
         );
         return res.status(200).json({
             message: 'Senha alterada com sucesso'
@@ -71,9 +108,16 @@ class UserController {
 
   async destroy(req, res) {
     try {
-        // userId provisório, deve vir do token
-        // req.user.id
-        await UserService.deleteUser(req.params.id, 1);
+        const id = req.params.id;
+        const idValidation = UserValidators.isValidId(id);
+        if (!idValidation.isValid) {
+            return res.status(400).json({
+                message: 'ID inválido',
+                errors: idValidation.errors
+            });
+        }
+
+        await UserService.deleteUser(id, req.user.id);
         return res.status(200).json({
             message: 'Usuário deletado com sucesso'
         });
--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -5,26 +5,6 @@ import AppError from '../utils/AppError.js';
 class UserService {
 
   async createUser(user) {
-    //Criar middleware para fazer a validação
-    const hasUpperCase = /[A-Z]/.test(user.password);
-    const hasLowerCase = /[a-z]/.test(user.password);
-    const hasNumber = /[0-9]/.test(user.password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(user.password);
-    const hasMinLength = user.password.length >= 8;
-    if (!hasMinLength) throw new AppError('A senha deve ter pelo menos 8 caracteres');
-    if (!hasUpperCase) throw new AppError('A senha deve ter pelo menos uma letra maiúscula');
-    if (!hasLowerCase) throw new AppError('A senha deve ter pelo menos uma letra minúscula');
-    if (!hasNumber) throw new AppError('A senha deve ter pelo menos um número');
-    if (!hasSpecialChar) throw new AppError('A senha deve ter pelo menos um caractere especial');
-    if (!user.first_name || !user.last_name || !user.email || !user.password) throw new AppError('Nome, sobrenome, e-mail e senha são obrigatórios');
-    if (user.first_name.length < 3) throw new AppError('O nome deve ter pelo menos 3 caracteres');
-    if (user.last_name.length < 3) throw new AppError('O sobrenome deve ter pelo menos 3 caracteres');
-    if (!user.email.includes('@') || !user.email.includes('.') || user.email.length < 5) throw new AppError('O e-mail deve ser válido');
-    if (user.first_name.length > 50) throw new AppError('O nome deve ter no máximo 50 caracteres');
-    if (user.last_name.length > 50) throw new AppError('O sobrenome deve ter no máximo 50 caracteres');
-    if (user.email.length > 100) throw new AppError('O e-mail deve ter no máximo 100 caracteres');
-    if (user.password.length > 100) throw new AppError('A senha deve ter no máximo 100 caracteres');
-    
     const userExists = await UserRepository.findByEmailWithPassword(user.email);
     if (userExists) throw new AppError('E-mail já está em uso', 409);
 
@@ -47,11 +27,6 @@ class UserService {
   };
 
   async findAllWithPagination(name, page, limit) {
-    //Criar middleware para fazer a validação
-    name = typeof name === 'string' ? name : '';
-    page = parseInt(page, 10) || 1;
-    limit = parseInt(limit, 10) || 5;
-
     const offset = (page - 1) * limit;
     const users = await UserRepository.findAllWithPagination( name, limit, offset );
     if (!users || users.length === 0) throw new AppError('Erro ao buscar usuários com paginação', 500);
@@ -71,9 +46,6 @@ class UserService {
   };
   
   async findById(id) {
-    //Criar middleware para fazer a validação
-    if (isNaN(Number(id)) || Number(id) <= 0) throw new AppError('ID do usuário é obrigatório');
-
     const user = await UserRepository.findById(id);
     if (!user) throw new AppError('Usuário não encontrado', 404);
 
@@ -81,11 +53,6 @@ class UserService {
   };
 
   async updateUser(user, id) {
-    //Criar middleware para fazer a validação
-    if (isNaN(Number(id)) || Number(id) <= 0) throw new AppError('ID do usuário é obrigatório');
-    if (!user.first_name && !user.last_name && !user.email) throw new AppError('Nome, sobrenome e e-mail são obrigatórios');
-    if (user.password) throw new AppError('A senha não pode ser atualizada aqui');
-    
     const result = await UserRepository.update(user, id);
     if (!result) throw new AppError('Erro ao atualizar usuário', 500);
 
@@ -95,23 +62,7 @@ class UserService {
     return updatedUser;
   };
 
-  async updatePassword(id, currentPassword, newPassword) {
-    //criar middleware para fazer a validação
-    if (isNaN(Number(id)) || Number(id) <= 0) throw new AppError('ID do usuário é obrigatório');
-    if (!currentPassword || !newPassword) throw new AppError('Senha atual e nova senha são obrigatórias');
-    if (currentPassword === newPassword) throw new AppError('A nova senha não pode ser igual à senha atual');
-    const hasUpperCase = /[A-Z]/.test(newPassword);
-    const hasLowerCase = /[a-z]/.test(newPassword);
-    const hasNumber = /[0-9]/.test(newPassword);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(newPassword);
-    const hasMinLength = newPassword.length >= 8;
-    if (!hasMinLength) throw new AppError('A senha deve ter pelo menos 8 caracteres');
-    if (!hasUpperCase) throw new AppError('A senha deve ter pelo menos uma letra maiúscula');
-    if (!hasLowerCase) throw new AppError('A senha deve ter pelo menos uma letra minúscula');
-    if (!hasNumber) throw new AppError('A senha deve ter pelo menos um número');
-    if (!hasSpecialChar) throw new AppError('A senha deve ter pelo menos um caractere especial');
-    
-    
+  async updatePassword(id, currentPassword, newPassword) {    
     const user = await UserRepository.findByIdWithPassword(id);
     if (!user) throw new AppError('Usuário não encontrado', 404);
     
@@ -125,11 +76,7 @@ class UserService {
     return;
   };
 
-  async deleteUser(id, deletedBy) {
-    //Criar middleware para fazer a validação
-    if (isNaN(Number(id)) || Number(id) <= 0) throw new AppError('ID do usuário é obrigatório');
-    if (!deletedBy) throw new AppError('ID do usuário que está deletando é obrigatório');
-    
+  async deleteUser(id, deletedBy) {    
     const result = await UserRepository.softDelete(id, deletedBy);
     if (!result) throw new AppError('Erro ao deletar usuário', 500);
 

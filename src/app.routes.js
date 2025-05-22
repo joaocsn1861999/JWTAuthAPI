6a877b48612ab59d03fa26b30c5f10527864e201
João Carlos <joaocarlosciclovida@gmail.com>
2025-05-22 16:10:48 -0300

    feat: implementado token validator nas rotas users

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userRouter from './app/routes/user.routes.js';
 import loginRouter from './app/routes/login.routes.js';
+import tokenValidator from './app/middlewares/tokenValidator.js';
 
 const appRouter = Router();
 
-appRouter.use('/users', userRouter);
+appRouter.use('/users', tokenValidator, userRouter);
 appRouter.use('/login', loginRouter);
 
 appRouter.use('/helloWorld', (req, res) => {

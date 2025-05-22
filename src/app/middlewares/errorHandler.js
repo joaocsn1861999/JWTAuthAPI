69f737d0f361146bb728ded4bfebbc2b769125e0
João Carlos <joaocarlosciclovida@gmail.com>
2025-05-22 16:16:06 -0300

    removido console.log teste

--- src/app/middlewares/errorHandler.js
+++ src/app/middlewares/errorHandler.js
@@ -1,6 +1,4 @@
 export default function errorHandler(error, req, res, next) {
-  //Para desenvolvimento
-  console.error(error);
   
   const statusCode = error.statusCode || 500;
   const message = error.message || 'Erro interno do servidor';

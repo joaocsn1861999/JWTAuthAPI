b7a27ac982f83ea2a347f19fce40dafd78d71bf5
João Carlos <joaocarlosciclovida@gmail.com>
2025-05-23 14:45:43 -0300

    refactor: refatorado middleware adminUserCheck

--- src/app/middlewares/adminUserCheck.js
+++ src/app/middlewares/adminUserCheck.js
@@ -1,6 +1,7 @@
 export default function adminUserCheck(req, res, next) {
-  if (req.user.is_admin) next();
-  return res.status(401).json({
-    message: "Você não tem permissão para acessar este recurso",
-  });
+  if (!req.user.is_admin)
+    return res.status(401).json({
+      message: "Você não tem permissão para acessar este recurso",
+    });
+  next();
 }

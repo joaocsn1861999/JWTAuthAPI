1ddfbfcb8a57c5e8f66d35db1f890187f80eed11
João Carlos <joaocarlosciclovida@gmail.com>
2025-08-12 09:17:45 -0300

    update: routes updated to dependency injection

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,11 +1,25 @@
 import { Router } from 'express';
-import userRouter from './app/routes/user.routes.js';
-import loginRouter from './app/routes/login.routes.js';
+import { makeInvoker } from 'awilix-express';
+import LoginController from '../controllers/LoginController.js';
+import UserController from '../controllers/UserController.js';
+import adminUserCheck from '../middlewares/adminUserCheck.js';
+import tokenValidator from '../middlewares/tokenValidator.js';
 
+const loginController = makeInvoker(LoginController);
+const userController = makeInvoker(UserController);
 const appRouter = Router();
 
-appRouter.use('/users', userRouter);
-appRouter.use('/login', loginRouter);
+appRouter.post('/login', loginController('login'));
+
+appRouter.get('/users', tokenValidator, userController('index'));
+appRouter.get('/users/count', tokenValidator, userController('count'));
+appRouter.get('/users/:id', tokenValidator, userController('show'));
+appRouter.post('/users', userController('store'));
+appRouter.patch('/users/me/password', tokenValidator, userController('changePassword'));
+appRouter.patch('/users/me', tokenValidator, userController('update'));
+appRouter.patch('/users/:id', tokenValidator, adminUserCheck, userController('update'));
+appRouter.delete('/users/me', tokenValidator, userController('destroy'));
+appRouter.delete('/users/:id', tokenValidator, adminUserCheck, userController('destroy'));
 
 appRouter.use('/helloWorld', (req, res) => {
     return res.status(200).json({

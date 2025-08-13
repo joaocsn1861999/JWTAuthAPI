b8f272ed10431c39de9b4c0612625d3a62cd1440
João Carlos <joaocarlosciclovida@gmail.com>
2025-08-13 14:40:19 -0300

    fix: wrong import

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { makeInvoker } from 'awilix-express';
-import LoginController from '../controllers/LoginController.js';
-import UserController from '../controllers/UserController.js';
-import adminUserCheck from '../middlewares/adminUserCheck.js';
-import tokenValidator from '../middlewares/tokenValidator.js';
+import LoginController from './app/controllers/LoginController.js';
+import UserController from './app/controllers/UserController.js';
+import adminUserCheck from './app/middlewares/adminUserCheck.js';
+import tokenValidator from './app/middlewares/tokenValidator.js';
+import { meScope, otherUserScope } from './app/middlewares/requestScope.js'
 
 const loginController = makeInvoker(LoginController);
 const userController = makeInvoker(UserController);
@@ -16,10 +17,10 @@ appRouter.get('/users/count', tokenValidator, userController('count'));
 appRouter.get('/users/:id', tokenValidator, userController('show'));
 appRouter.post('/users', userController('store'));
 appRouter.patch('/users/me/password', tokenValidator, userController('changePassword'));
-appRouter.patch('/users/me', tokenValidator, userController('update'));
-appRouter.patch('/users/:id', tokenValidator, adminUserCheck, userController('update'));
-appRouter.delete('/users/me', tokenValidator, userController('destroy'));
-appRouter.delete('/users/:id', tokenValidator, adminUserCheck, userController('destroy'));
+appRouter.patch('/users/me', tokenValidator, meScope, userController('update'));
+appRouter.patch('/users/:id', tokenValidator, otherUserScope, adminUserCheck, userController('update'));
+appRouter.delete('/users/me', tokenValidator, meScope, userController('destroy'));
+appRouter.delete('/users/:id', tokenValidator, otherUserScope, adminUserCheck, userController('destroy'));
 
 appRouter.use('/helloWorld', (req, res) => {
     return res.status(200).json({
